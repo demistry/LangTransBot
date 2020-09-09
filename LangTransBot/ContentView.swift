0f8d986fa4039dc468970fa3957224f7f6c42d80
David Ilenwabor <dave11ilen@gmail.com>
2020-09-09 02:13:24 +0100

    Initial Commit

---
+++ LangTransBot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LangTransBot
+//
+//  Created by David Ilenwabor on 09/09/2020.
+//  Copyright © 2020 Davidemi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
